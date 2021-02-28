ff5b9ed5dd8b2653722ca0c72ff1ec521624915b
antoinemassih <antoine.amassih@gmail.com>
2021-02-28 18:33:00 +0400

    Mac Pro Commit

--- DBUtils/OneTimeUtils/test.py
+++ DBUtils/OneTimeUtils/test.py
@@ -1,7 +1,9 @@
-import datetime
-import time
-from DBUtils.databaseConnect import DBConnect as DB
+from DataTransformUtils.DateTimeUtils.DateProcessor import ExpandDate
+from DataTransformUtils.TradeDataUtils.TradeSymbolParser import ETTradeSymbolParser
 
-DBConnection = DB(['TradeHistory'])
-print(DBConnection.readDB('TradeHistory').fetchmany(50))
+#DBConnection = DB('TradeData,['TradeHistory'])
+#print(DBConnection.readDB('TradeHistory').fetchmany(50))
 
+
+print(ExpandDate('19/02/2021',"%d/%m/%Y"))
+print(ETTradeSymbolParser("ADBE May 15 '20 $350 Call"))
